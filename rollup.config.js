d1800a450c64e0cda92272e52f9ddbc0eb0127a2
Alexander Patin <alex@squarelink.com>
2019-05-17 12:33:59 -0400

    updated rollup to build bundle and package

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ import replace from 'rollup-plugin-replace'
 import builtins from 'rollup-plugin-node-builtins'
 
 // list of plugins used during building process
-const plugins = targets => ([
+const bundlePlugins = targets => ([
   replace({
     exclude: 'node_modules/**',
     values: {
@@ -31,8 +31,35 @@ const plugins = targets => ([
   builtins()
 ])
 
+const packagePlugins = targets => ([
+  replace({
+    exclude: 'node_modules/**',
+    values: {
+      '<@ENVIRONMENT@>': 'production',
+      '<@VERSION@>': '0.1.1'
+    },
+    delimiters: ['', '']
+  }),
+  babel({
+    exclude: ['node_modules/**'],
+    presets: [['env', { modules: false }]],
+    plugins: ['external-helpers', 'babel-plugin-transform-object-rest-spread'],
+    comments: false,
+  })
+])
+
 // packages that should be treated as external dependencies, not bundled
-const external = ['fetch-ponyfill', 'squarelink-provider-engine'] // e.g. ['axios']
+const external = [
+  'fetch-ponyfill',
+  'squarelink-provider-engine',
+  'squarelink-provider-engine/subproviders/cache',
+  'squarelink-provider-engine/subproviders/fixture',
+  'squarelink-provider-engine/subproviders/filters',
+  'squarelink-provider-engine/subproviders/vm',
+  'squarelink-provider-engine/subproviders/hooked-wallet',
+  'squarelink-provider-engine/subproviders/nonce-tracker',
+  'squarelink-provider-engine/subproviders/rpc',
+] // e.g. ['axios']
 
 export default [{
   // source file / entrypoint
@@ -40,7 +67,7 @@ export default [{
   // output configuration
   output: {
     // name visible for other scripts
-    name: 'npmLibPackageExample',
+    name: 'squarelink',
     // output file location
     file: 'lib/squarelink.min.js',
     // format of generated JS file, also: esm, and others are available
@@ -49,6 +76,14 @@ export default [{
     sourcemap: true,
   },
   external,
-  // build es modules for node 8
-  plugins: plugins({ node: '10' }),
+  plugins: bundlePlugins({ node: '8' }),
+}, {
+  input: 'src/index.js',
+  output: {
+    name: 'squarelink',
+    file: 'dist/index.js',
+    format: 'cjs'
+  },
+  external,
+  plugins: packagePlugins({ node: '8' })
 }]
