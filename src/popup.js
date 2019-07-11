6e150916395c5013e07308b0f67e3a7f60167bae
Alexander Patin <alex@squarelink.com>
2019-07-10 23:51:04 -0400

    got postMessage communication working for popup method

--- src/popup.js
+++ src/popup.js
@@ -2,28 +2,48 @@ import Iframe from './iframe'
 
 const POPUP_PARAMS = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=375,height=350,left=-500,top=150`
 
-const getPopup = (url) => {
+const getPopup = ({ url, params }) => {
   var popup = window.open('', '_blank', POPUP_PARAMS)
   if(!popup || popup.closed || typeof popup.closed=='undefined')
     return Promise.resolve({ iframe: new Iframe(url) })
   // Check if popup is blocked on Chrome
-  return _chromeCheck({ popup, url })
+  return _chromeCheck({ popup, url, params })
 }
 
-const _chromeCheck = ({ popup, url }) =>
+const _chromeCheck = ({ popup, url, params }) =>
   new Promise((resolve, reject) => {
+    var result = false
     popup.onload = () => {
       setTimeout(() => {
+        if (result) return
+        result = true
         if (!popup.innerHeight || popup.innerHeight <= 0) {
           popup.close()
           resolve({ iframe: new Iframe(url) })
         } else {
-          popup.location.href = url
-          popup.focus()
-          resolve({ popup })
+          _loadPopup({ url, popup, params }).then(popup => {
+            resolve({ popup })
+          })
         }
       }, 1)
     }
+  })
+
+const _loadPopup = ({ url, popup, params }) =>
+  new Promise((resolve, reject) => {
+    popup.location.href = url
+    var result = false
+    window.addEventListener('message', (e) => {
+      if (result) return
+      const { origin, type } = e.data
+      if (origin === 'squarelink' && type === 'onload') {
+        result = true
+        popup.postMessage({ origin: 'squarelink-web3-sdk', params }, '*')
+        popup.focus()
+        resolve(popup)
+        window.removeEventListener('message', function() {})
+      }
+    }, false)
   })
 
 export default getPopup
